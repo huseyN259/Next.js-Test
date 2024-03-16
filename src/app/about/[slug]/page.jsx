19e6f82a8dc0dd00911d2c1036b54eca575d47cd
Ibra_xj86 <Ibra_xj86@itstep.edu.az>
2024-03-16 05:50:15 +0400

    Test

---
+++ src/app/about/[slug]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DetailPage = () => {
+  return (
+    <div>Details Page</div>
+  )
+}
+
+export default DetailPage;
---
+++ src/app/about/page.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Hello from '../components/about/Hello';
+
+const About = () => {
+  return <Hello />;
+}
+
+export default About;
